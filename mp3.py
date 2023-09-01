9117f2ab89ad24aa25269ec22c9bb6aec923e714
nazir360 <ransfordtakyi149@gmail.com>
2023-09-01 09:29:38 +0000

    last commit

--- mp3.py
+++ mp3.py
@@ -1,67 +1,77 @@
-
 import random
 
-class Mp3_Playlist:
-    def __init__(self,playlist_name,song):
-        self.playlist_name = playlist_name
-        self.tracks = []
-        self. song = song
-        
 
+class MP3_playlist:
+    # The constructor initialises the music attribute as an empty list to store music related data.
+    def __init__(self):
+        self.Music = []
 
+    # This method opens a supposed music file in read mode,and loads the tracks into the Music array.
+    def load_playlist(self, Music_file):
+        try:
+            with open(Music_file, "r") as file:
+                self.Music = [line.strip() for line in file.readlines()]
+            print(f"Loaded {len(self.Music)} tracks from {Music_file}")
+        except FileNotFoundError:
+            print("File not found.")
 
-        
-    
-    def display_Tracks(self):
-         self.tracks = []
-
-    def enqueue_track(self,song):
-         return self.tracks.append(song)
-
-    def remove_track(self,song):
-        if song in self.tracks:
-            self.tracks.remove(song)
+    # This method checks the content of the self.Music and if it is not empty it prints its content.
+    def display_playlist(self):
+        if not self.Music:
+            print("playlist is empty.")
         else:
-            print(f"{song} not found in the playlist.")
-        
-        
-    def save_playlist_to_file(self, filename):
-        with open(filename, "w") as fhand:
-            for song in self.tracks:
-                 fhand.write(song + "\n")
-        
-    def shuffle_songs(self):
-         random.shuffle(self.tracks)
-         print(self.tracks)
-    def count_tracks(self):
-        return len(self.tracks)
+            print("Current PLaylist:")
+            for index, track in enumerate(self.Music, start=1):
+                print(f"{index}. {track}")
 
-    def calculate_total_duration(self):
-       
-        total_duration = 0
-        for track in self.tracks:
-            total_duration += track.duration
-            return total_duration
-   
+    # This method allows you to add tracks to the playlist by appending whatever track to self.Music.
+    def enqueue_track(self, track):
+        self.Music.append(track)
+        print(f"Added '{track}' to the playlist.")
+
+    def remove_track(self, track):
+        if track in self.Music:
+            self.Music.remove(track)
+            print(f"Removed '{track}' from the playlist.")
+        else:
+            print(f"'{track}' not found in the playlist.")
+    # Opens the supppose file in write mode and stores the tracks in the self.music as a txt file.
+
+    def save_playlist(self, Music_file):
+        with open(Music_file, 'w') as file:
+            file.write('\n'.join(self.Music))
+        print(f"Playlist saved to {Music_file}.")
+
+    def shuffle_playlist(self):
+        random.shuffle(self.Music)
+        print("Playlist shuffled.")
+
+    def count_tracks(self):
+        return len(self.Music)
+
+    def calculate_duration(self):
+        # Assume each track is 2 minutes
+        track_duration = 2  # in minutes
+        total_duration = len(self.Music) * track_duration
+        return total_duration
 
     def clear_playlist(self):
-        self.tracks.clear()
-
+        self.Music = []
+        print("Playlist cleared.")
 
     def is_empty(self):
-        return len(self.tracks) == 0 
-
-playlist1 = Mp3_Playlist("my_playlist","sugar")
-playlist1.enqueue_track("Hit em up.mp3")
-playlist1.enqueue_track("mama.mp3")
-playlist1.enqueue_track("ambitionz az a ridah.mp3")
-playlist1.save_playlist_to_file("music.txt")
+        return len(self.Music) == 0
 
 
-print("\n...shuffling...\n")
-print(playlist1.shuffle_songs())
-playlist1.save_playlist_to_file("music.txt")
-print(playlist1.count_tracks())
-print(playlist1.is_empty())
-print(playlist1.display_Tracks())
-print(playlist1.remove_track('mama.mp3'))
+# Creating an instance of the MP3_playlist class
+playlist = MP3_playlist()
+playlist.load_playlist("playlist.txt")
+playlist.display_playlist()
+playlist.enqueue_track("song 1")
+playlist.enqueue_track("song 2")
+playlist.enqueue_track("song 3")
+
+playlist.remove_track("song 3")
+playlist.display_playlist()
+playlist.clear_playlist()
+playlist.display_playlist()
